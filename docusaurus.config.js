ee237477b2d02ded8b310a02332bd0cd9f1776ac
Adriano Ruseler <adrianoruseler@gmail.com>
2025-08-28 12:16:49 -0300

    Update Copyright

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -313,7 +313,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Moodle Docs, Inc. Built with Docusaurus.`,
       },
       prism: {
         theme: prismThemes.github,
